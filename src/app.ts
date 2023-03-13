87078109111a8759205c35330bc43a46fdcc58a6
claramarsango <claraxmarsango@gmail.com>
2023-03-13 17:11:47 +0100

    create express app with cors origin and test

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
-export const hello = {
-  hello: 'Hello world',
-};
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(cors({ origin: ['http://localhost:4000/'] }));
+app.get('/', (req, res) => {
+  res.json('server is up!');
+});
+app.use(express.json());
+
+app.use(bodyParser.json());
+
+export default app;
