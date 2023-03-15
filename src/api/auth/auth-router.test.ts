d453eecb475ecfc65a1b9c68708e978fbf161f30
claramarsango <claraxmarsango@gmail.com>
2023-03-15 21:35:35 +0100

    test custom error handler

--- src/api/auth/auth-router.test.ts
+++ src/api/auth/auth-router.test.ts
@@ -4,6 +4,8 @@ import mongoose from 'mongoose';
 import connectDB from '../../database/mongoDB.js';
 import { AuthRequest } from '../../types/auth-types.js';
 import app from '../../app.js';
+import { encryptPassword } from './auth-utils.js';
+import { UserModel } from '../users/user-model.js';
 
 describe('Given an app with an auth router', () => {
   let mongoServer: MongoMemoryServer;
@@ -62,20 +64,33 @@ describe('Given an app with an auth router', () => {
   });
 
   describe('When a user wants to login with a valid email and password,', () => {
+    const nonExistentUser = {
+      email: 'mock@email.com',
+      password: 'mockPassword',
+    };
     test('and the password encryption key environment variable does not exist, then it should return a 500 error', async () => {
-      const user = {
-        email: 'mock@email.com',
-        password: 'mockPassword',
-      };
-
       delete process.env.PASSWORD_ENCRYPTION_KEY;
 
       const response = await request(app)
         .post('/auth/login')
-        .send(user)
+        .send(nonExistentUser)
         .expect(500);
 
       expect(response.status).toEqual(500);
+    });
+
+    test('and the user does not exist, then it should return a 404 error', async () => {
+      await request(app).post('/auth/login').send(nonExistentUser).expect(404);
+    });
+
+    test('and the user exists, then it should be logged in', async () => {
+      const userDb = {
+        ...nonExistentUser,
+        password: encryptPassword(nonExistentUser.password),
+      };
+      await UserModel.create(userDb);
+
+      await request(app).post('/auth/login').send(nonExistentUser).expect(201);
     });
   });
 });
