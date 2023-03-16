51770dad9f46cc579169ad86f2bf5980a1bd5d29
claramarsango <claraxmarsango@gmail.com>
2023-03-16 14:19:39 +0100

    add register controller to register route in auth router

--- src/api/auth/auth-router.ts
+++ src/api/auth/auth-router.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import { validate } from 'express-validation';
-import { loginUserController } from './auth-controllers.js';
+import {
+  loginUserController,
+  registerUserController,
+} from './auth-controllers.js';
 import { entryValidation } from './entry-validation.js';
 
 const authRouter = express.Router();
 
 authRouter.use(validate(entryValidation));
 
+authRouter.route('/register').post(registerUserController);
 authRouter.route('/login').post(loginUserController);
 
 export default authRouter;
