6af862cca46883be84da46304f49f78b1f84f602
claramarsango <claraxmarsango@gmail.com>
2023-03-16 18:20:44 +0100

    create login and register validtions

--- src/api/auth/entry-validation.ts
+++ src/api/auth/entry-validation.ts
@@ -1,9 +1,21 @@
 import { Joi } from 'express-validation';
 
-export const entryValidation = {
+export const loginValidation = {
   body: Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string()
+      .regex(/[a-zA-Z0-9]{3,30}/)
+      .required(),
+  }),
+};
+
+export const registerValidation = {
+  body: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .regex(/[a-zA-Z0-9]{3,30}/)
+      .required(),
+    confirmedPassword: Joi.string()
       .regex(/[a-zA-Z0-9]{3,30}/)
       .required(),
   }),
