b3b147bf69d7bdc1a30970557e17c607d82dcf59
claramarsango <claraxmarsango@gmail.com>
2023-03-17 14:10:12 +0100

    create api router route in ap

--- src/app.ts
+++ src/app.ts
@@ -3,22 +3,22 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import authRouter from './api/auth/auth-router.js';
 import errorHandler from './errors/error-handler.js';
+import apiRouter from './api/api-router.js';
 
 const app = express();
 
-app.disable('x-powered-by');
-
 app.use(cors());
+app.disable('x-powered-by');
 
 app.get('/', (req, res) => {
   res.json('server is up!');
 });
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.use('/auth', authRouter);
-
-app.use(bodyParser.json());
+app.use('/api/v1', apiRouter);
 
 app.use(errorHandler);
 
