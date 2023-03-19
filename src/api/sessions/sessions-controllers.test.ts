8f3bf0fbbcb1bc3224d85ddd297c100c62e49328
claramarsango <claraxmarsango@gmail.com>
2023-03-19 15:07:19 +0100

    test create session controller with unit and integrated tests

---
+++ src/api/sessions/sessions-controllers.test.ts
@@ -0,0 +1,123 @@
+import { Request, Response } from 'express';
+import { Session, SessionModel } from './session-model';
+import {
+  createSessionController,
+  SessionRequest,
+} from './sessions-controllers';
+
+jest.mock('@supabase/supabase-js', () => {
+  const data = {
+    publicUrl: 'https://example.com/photo.webp',
+  };
+  return {
+    createClient: jest.fn().mockImplementation(() => ({
+      storage: {
+        from: jest.fn().mockReturnValue({
+          upload: jest.fn().mockResolvedValue({
+            error: null,
+            data: {
+              ...data,
+            },
+          }),
+          getPublicUrl: jest.fn().mockReturnValue({
+            error: null,
+            data: {
+              ...data,
+            },
+          }),
+        }),
+      },
+    })),
+  };
+});
+
+describe('Given a controller to create sessions,', () => {
+  const mockResponse = {
+    json: jest.fn(),
+    status: jest.fn().mockReturnThis(),
+    locals: { id: 'mockId' },
+  } as Partial<Response<Session, { id: string }>>;
+
+  const next = jest.fn();
+
+  const session = {
+    title: 'mockSession',
+    coverImageURL: 'https://example.com/photo.webp',
+    url: '',
+    queuedSongs: [],
+    admin: 'mockUserId',
+    participants: [],
+    _id: 'mockSessionId',
+  };
+
+  SessionModel.create = jest.fn().mockResolvedValue(session);
+
+  test('when the user tries to create a session without a title, it should pass on an error', async () => {
+    const invalidMockRequest = {
+      body: {
+        title: '',
+      },
+      file: { buffer: Buffer.from('mockBuffer') },
+    } as unknown as Partial<Request>;
+
+    await createSessionController(
+      invalidMockRequest as Request<
+        unknown,
+        Session,
+        SessionRequest,
+        unknown,
+        { id: string }
+      >,
+      mockResponse as Response<Session, { id: string }>,
+      next,
+    );
+
+    expect(next).toHaveBeenCalled();
+  });
+
+  test('when the user does not upload an image, the session should be created with a default image', async () => {
+    const invalidMockRequest = {
+      body: {
+        title: 'mockTitle',
+      },
+      file: { buffer: undefined },
+    } as unknown as Partial<Request>;
+
+    await createSessionController(
+      invalidMockRequest as Request<
+        unknown,
+        Session,
+        SessionRequest,
+        unknown,
+        { id: string }
+      >,
+      mockResponse as Response<Session, { id: string }>,
+      next,
+    );
+
+    expect(mockResponse.status).toHaveBeenCalledWith(201);
+  });
+
+  test('when the user uploads an image and provides a title, it should create a session', async () => {
+    const mockRequest = {
+      body: {
+        title: 'sessionTitle',
+      },
+      file: { buffer: Buffer.from('mockBuffer') },
+    } as Partial<Request>;
+
+    await createSessionController(
+      mockRequest as Request<
+        unknown,
+        Session,
+        SessionRequest,
+        unknown,
+        { id: string }
+      >,
+      mockResponse as Response<Session, { id: string }>,
+      next,
+    );
+
+    expect(mockResponse.status).toHaveBeenCalledWith(201);
+  });
+});
---
+++ src/api/sessions/sessions-router.test.ts
@@ -0,0 +1,84 @@
+import request from 'supertest';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import app from '../../app';
+import connectDB from '../../database/mongoDB';
+import { generateJWTToken } from '../auth/auth-utils';
+
+jest.mock('@supabase/supabase-js', () => {
+  const data = {
+    publicUrl: 'https://example.com/photo.webp',
+  };
+  return {
+    createClient: jest.fn().mockImplementation(() => ({
+      storage: {
+        from: jest.fn().mockReturnValue({
+          upload: jest.fn().mockResolvedValue({
+            error: null,
+            data: {
+              ...data,
+            },
+          }),
+          getPublicUrl: jest.fn().mockReturnValue({
+            error: null,
+            data: {
+              ...data,
+            },
+          }),
+        }),
+      },
+    })),
+  };
+});
+
+describe('Given a sessions router,', () => {
+  let mongoServer: MongoMemoryServer;
+  const OLD_ENV = process.env;
+
+  beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const mongoUrl = mongoServer.getUri();
+    await connectDB(mongoUrl);
+  });
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...OLD_ENV };
+  });
+
+  afterAll(async () => {
+    await mongoServer.stop();
+    await mongoose.connection.close();
+    process.env = OLD_ENV;
+  });
+
+  const sessionMockRequest = {
+    body: {
+      title: 'sessionTitle',
+    },
+    file: { buffer: undefined },
+  };
+
+  const mockToken = generateJWTToken('123456789123456789123456');
+
+  test('when the user has a token, then they should be able to create a session', async () => {
+    await request(app)
+      .post('/api/v1/sessions/create')
+      .send(sessionMockRequest.body)
+      .set('Authorization', `Bearer ${mockToken}`)
+      .expect(201);
+  });
+
+  test('when the user does not have a token, then they should not be able to create a session', async () => {
+    await request(app).get('/api/v1/sessions').expect(401);
+  });
+
+  test('when the jwt environment variable does not exist, the server should respond with a 500 error', async () => {
+    delete process.env.JWT_SECRET;
+
+    await request(app)
+      .post('/api/v1/sessions/create')
+      .set('Authorization', `Bearer ${mockToken}`)
+      .expect(500);
+  });
+});
